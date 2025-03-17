564cc09233b7502eba004531cea0aa4e1cd3e449
Ines Amirat <inesamirat20@gmail.com>
2025-03-17 21:39:32 +0100

    products again

--- src/app/modules/product/products/products.component.ts
+++ src/app/modules/product/products/products.component.ts
@@ -7,18 +7,35 @@ import { Component } from '@angular/core';
 })
 export class ProductsComponent {
       products = [
-        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/photo25.jpg"},
-        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/photo25.jpg"},
-        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/photo25.jpg"},
-        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/photo25.jpg"},
-        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/photo25.jpg"},
-        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/photo25.jpg"},
-        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/photo25.jpg"},
-        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/photo25.jpg"},
-        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/photo25.jpg"},
-        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/photo25.jpg"},
-        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/photo25.jpg"},
-        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/photo25.jpg"},
+        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/img2.avif"},
+        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/img3.avif"},
+        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/img4.avif"},
+        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/img2.avif"},
+        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/img3.avif"},
+        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/img4.avif"},
+        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/img2.avif"},
+        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/img3.avif"},
+        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/img4.avif"},
+        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/img2.avif"},
+        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/img3.avif"},
+        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/img4.avif"},
+        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/img2.avif"},
+        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/img3.avif"},
+        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/img4.avif"},
+        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/img2.avif"},
+        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/img3.avif"},
+        {title:"grgrg" ,description:"rgrg",price:"rggrg" , imageURL :"assets/images/img4.avif"},
        
+       
+      ];
+
+
+      categories = [
+        {name: "Headphones"},
+        {name: "Keyboards and Mice"},
+        {name: "Smartwatches"},
+        {name: "Chargers and Batteries"},
+        {name: "PC Accessories"},
+        {name: "Smartphone Accessories"},
       ];
 }
